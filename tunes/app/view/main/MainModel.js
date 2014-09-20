065d6218020db51b931516c92ca5a88aaf0ff603
stoe <stefan@stoelzle.me>
2014-09-20 10:17:13 +1000

    "Tunes" Ext JS 5 starter app.
     * new file:   tunes/.sencha/app/Boot.js
     * new file:   tunes/.sencha/app/Microloader.js
     * new file:   tunes/.sencha/app/app.defaults.json
     * new file:   tunes/.sencha/app/bootstrap-impl.xml
     * new file:   tunes/.sencha/app/build-impl.xml
     * new file:   tunes/.sencha/app/build.properties
     * new file:   tunes/.sencha/app/codegen.json
     * new file:   tunes/.sencha/app/cordova-impl.xml
     * new file:   tunes/.sencha/app/cordova.defaults.properties
     * new file:   tunes/.sencha/app/defaults.properties
     * new file:   tunes/.sencha/app/development.defaults.properties
     * new file:   tunes/.sencha/app/development.properties
     * new file:   tunes/.sencha/app/ext.properties
     * new file:   tunes/.sencha/app/find-cmd-impl.xml
     * new file:   tunes/.sencha/app/init-impl.xml
     * new file:   tunes/.sencha/app/js-impl.xml
     * new file:   tunes/.sencha/app/microloader/development.js
     * new file:   tunes/.sencha/app/microloader/production.js
     * new file:   tunes/.sencha/app/microloader/testing.js
     * new file:   tunes/.sencha/app/native.defaults.properties
     * new file:   tunes/.sencha/app/native.properties
     * new file:   tunes/.sencha/app/package.defaults.properties
     * new file:   tunes/.sencha/app/package.properties
     * new file:   tunes/.sencha/app/packager-impl.xml
     * new file:   tunes/.sencha/app/page-impl.xml
     * new file:   tunes/.sencha/app/phonegap-impl.xml
     * new file:   tunes/.sencha/app/phonegap.defaults.properties
     * new file:   tunes/.sencha/app/plugin.xml
     * new file:   tunes/.sencha/app/production.defaults.properties
     * new file:   tunes/.sencha/app/production.properties
     * new file:   tunes/.sencha/app/refresh-impl.xml
     * new file:   tunes/.sencha/app/resolve-impl.xml
     * new file:   tunes/.sencha/app/resources-impl.xml
     * new file:   tunes/.sencha/app/sass-impl.xml
     * new file:   tunes/.sencha/app/sencha.cfg
     * new file:   tunes/.sencha/app/slice-impl.xml
     * new file:   tunes/.sencha/app/testing.defaults.properties
     * new file:   tunes/.sencha/app/testing.properties
     * new file:   tunes/.sencha/app/watch-impl.xml
     * new file:   tunes/Readme.md
     * new file:   tunes/app.js
     * new file:   tunes/app.json
     * new file:   tunes/app/Application.js
     * new file:   tunes/app/Readme.md
     * new file:   tunes/app/model/Readme.md
     * new file:   tunes/app/store/Readme.md
     * new file:   tunes/app/view/main/Main.js
     * new file:   tunes/app/view/main/MainController.js
     * new file:   tunes/app/view/main/MainModel.js
     * new file:   tunes/bootstrap.css
     * new file:   tunes/bootstrap.js
     * new file:   tunes/bootstrap.json
     * new file:   tunes/build.xml
     * new file:   tunes/index.html
     * new file:   tunes/resources/Readme.md
     * new file:   tunes/sass/Readme.md
     * new file:   tunes/sass/config.rb
     * new file:   tunes/sass/example/bootstrap.css
     * new file:   tunes/sass/example/custom.js
     * new file:   tunes/sass/example/render.js
     * new file:   tunes/sass/example/theme.html

---
+++ tunes/app/view/main/MainModel.js
@@ -0,0 +1,14 @@
+/**
+ * This class is the view model for the Main view of the application.
+ */
+Ext.define('Tunes.view.main.MainModel', {
+    extend: 'Ext.app.ViewModel',
+
+    alias: 'viewmodel.main',
+
+    data: {
+        name: 'Tunes'
+    }
+
+    //TODO - add data, formulas and/or methods to support your view
+});
